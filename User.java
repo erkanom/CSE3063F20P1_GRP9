602ac7f95b5a20742d5ade74976335c3cee39805
suedabilen <suedabilen017@gmail.com>
2020-11-30 20:10:42 +0300

    Update user class
    user class update done

--- User.java
+++ User.java
@@ -1,14 +1,20 @@
-
+ 
 
 public class User{
 	private int id;
 	private String name;
 	private String userType;
+	private LabelMechanism mechanism;
+	private WorkSpace workSpace;
 	
 	public User(int id, String name, String userType) {
 		this.id=id;
 		this.name=name;
 		this.userType=userType;
+		if(userType.contentEquals("RandomBot")) {
+		mechanism=new RandomLabelingMechanism();
+		}
+		
 	}
 	
 	public void customs() {
@@ -40,4 +46,21 @@ public class User{
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
+
+	public LabelMechanism getMechanism() {
+		return mechanism;
+	}
+
+	public void setMechanism(LabelMechanism mechanism) {
+		this.mechanism = mechanism;
+	}
+
+	public WorkSpace getWorkSpace() {
+		return workSpace;
+	}
+
+	public void setWorkSpace(WorkSpace workSpace) {
+		this.workSpace = workSpace;
+	}
+
 }
