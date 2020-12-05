201a76dff88fd9a55c10b43bf0ab9f9ee1c36686
farouk949 <74612889+farouk949@users.noreply.github.com>
2020-12-05 14:45:22 +0300

    Updated classes

--- LabelMechanism.java
+++ LabelMechanism.java
@@ -1,16 +1,5 @@
 import java.util.ArrayList;
 
-
 public abstract class LabelMechanism {
-
-	
-
-	
-
-	public void doLabeling(WorkSpace workSpace, User user) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+	public abstract void  doLabeling(WorkSpace workSpace, User user);
 }
