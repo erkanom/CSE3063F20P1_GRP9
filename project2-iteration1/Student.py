1bd27f47e74ba152be27940ab50bf630b3d84954
suedabilen <suedabilen017@gmail.com>
2021-01-12 20:06:57 +0300

    Student Class
    update

--- project2-iteration1/Student.py
+++ project2-iteration1/Student.py
@@ -1,7 +1,41 @@
+
 class Student(object):
 
-    def __init__(self, studentNO, studentFirstName, studentLastName,studentInformation):
-        self.studentNO = studentNO
-        self.studentFirstName = studentFirstName
-        self.studentLastName = studentLastName
-        self.studentInformation = studentInformation
+    def __init__(self, StudentNO, StudentFirstName, StudentLastName, StudentInformation):
+        self.StudentNO = StudentNO
+        self.StudentFirstName = StudentFirstName
+        self.StudentLastName = StudentLastName
+        self.StudentInformation = StudentInformation
+
+    def getStudentNo(self):
+        return self.StudentNO
+
+    def setStudentNo(self,studentNo):
+        self.StudentNO = studentNo
+
+    def getStudentFirstName(self):
+            return self.studentFirstName
+
+    def setStudentFirstName(self, studentFirstName):
+            self.studentFirstName = studentFirstName
+
+    def getStudentLastName(self):
+            return self.StudentLastName
+
+    def setStudentLastName(self, StudentLastName):
+            self.StudentLastName = StudentLastName
+
+    def getStudentInformation(self):
+        return self.StudentInformation
+
+    def setStudentInformation(self, StudentInformation):
+        self.StudentInformation = StudentInformation
+
+
+
+
+
+
+
+
+
