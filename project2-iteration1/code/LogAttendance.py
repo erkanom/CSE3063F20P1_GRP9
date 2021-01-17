d1a0b18d998140d0f0a8f034e1cf8b18c91a87cf
ÖMER ERKAN <70490959+erkanom@users.noreply.github.com>
2021-01-17 21:43:42 +0300

    final

---
+++ project2-iteration1/code/LogAttendance.py
@@ -0,0 +1,10 @@
+class LogAttendance(object):
+
+    def __init__(self,submitDate):
+        self.submitDate = submitDate
+
+    def getSubmitDate(self):
+        return self.submitDate
+
+    def setSubmitDate(self, sumbitDate):
+        self.submitDate = sumbitDate
